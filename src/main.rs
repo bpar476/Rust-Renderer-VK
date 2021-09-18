269a03429ce01d5d6176da22d644002ace9411aa
Ben Partridge <ben.partridge249@gmail.com>
2021-09-18 20:36:36 +1000

    Validation Layers
    Create the vulkan instance with the "VK_LAYER_KHRONOS_validation"
    validation layer. We use enumerate_instance_layer_properties to load all
    global (instance) validation layers and assert that our required layers
    are present. Once sure we're present, we provide a pointer to an array
    of the validation layer names that we want to run with to the
    InstanceCreateInfo struct.
    
    Notes:
    * It may seem trivial to check if our required validation layers are
      provided by the vulkan driver. However, because the vulkan driver
      provides its strings as raw, null-terminated byte arrays we need to be very careful how
      we process strings. This commit adds a helper function to convert a
      raw vulkan string to a String. This is helpful when in CPU land,
      however when passing a string to the driver you need to be sure that
      the bytes you pass are a valid string and not accidentally a memory
      address. Using a &str (string slice) is the easiest way to guarantee
      your string values are bytes. Then using a CString to turn it into a
      null-terminated C string and getting the pointer from the C string.
      See the initialisation of `enabled_layers` in this commit for an
      example of converting strings in this way.
    * There are more validation layer steps - namely the message callback
      and other debugging utils. These will be in the next commit also
      titled Validation Layers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use core::panic;
 use std::ffi::CString;
+use std::os::raw::c_char;
+use std::string::FromUtf8Error;
 
 use ash::extensions::ext::DebugUtils;
 use ash::extensions::khr::{Surface, Win32Surface};
@@ -10,15 +13,34 @@ const APP_TITLE: &str = "Rust Renderer VK";
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 
+const VALIDATION_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
+
+fn read_vk_string(chars: &[c_char]) -> Result<String, FromUtf8Error> {
+    let terminator = '\0' as u8;
+    let mut content: Vec<u8> = vec![];
+
+    for raw in chars.iter() {
+        let ch = (*raw) as u8;
+
+        if ch != terminator {
+            content.push(ch);
+        } else {
+            break;
+        }
+    }
+
+    String::from_utf8(content)
+}
+
 struct HelloTriangleApplication {
     _entry: ash::Entry,
     instance: ash::Instance,
 }
 
 impl HelloTriangleApplication {
-    pub fn initialize() -> Self {
+    pub fn initialize(debug: bool) -> Self {
         let entry = unsafe { ash::Entry::new().unwrap() };
-        let instance = HelloTriangleApplication::create_instance(&entry);
+        let instance = HelloTriangleApplication::create_instance(&entry, debug);
         Self {
             _entry: entry,
             instance,
@@ -33,7 +55,39 @@ impl HelloTriangleApplication {
             .expect("Failed to create window.")
     }
 
-    fn create_instance(entry: &ash::Entry) -> ash::Instance {
+    fn assert_required_layers_available(entry: &ash::Entry) {
+        match entry.enumerate_instance_layer_properties() {
+            Ok(layers) => {
+                let layer_names: Vec<String> = layers
+                    .iter()
+                    .map(|layer| read_vk_string(&layer.layer_name[..]).unwrap())
+                    .collect();
+
+                layer_names
+                    .iter()
+                    .for_each(|layer| println!("Found validation layer {}", layer));
+
+                let mut unavailable_layers: Vec<&str> = vec![];
+                for layer in VALIDATION_LAYERS.iter() {
+                    if !layer_names.iter().any(|layer_name| layer_name.eq(layer)) {
+                        unavailable_layers.push(layer)
+                    }
+                }
+
+                if unavailable_layers.len() > 0 {
+                    unavailable_layers.iter().for_each(|&layer| {
+                        println!("Required validation layer {} is not available", layer)
+                    });
+
+                    panic!("Could not find required validation layers. See log for details.")
+                }
+            }
+            _ => panic!("Unable to load Vulkan validation layers"),
+        }
+    }
+
+    fn create_instance(entry: &ash::Entry, debug: bool) -> ash::Instance {
+        // Validate extensions
         let windows_required_extensions = vec![
             Surface::name().as_ptr(),
             Win32Surface::name().as_ptr(),
@@ -42,19 +96,30 @@ impl HelloTriangleApplication {
 
         // Check available extensions
         if let Ok(properties) = entry.enumerate_instance_extension_properties() {
+            // TODO check that required extensions are present
             properties
                 .iter()
-                .map(|property| {
-                    property
-                        .extension_name
-                        .iter()
-                        .map(|char| *char as u8)
-                        .collect()
-                })
-                .map(|name_raw| unsafe { CString::from_vec_unchecked(name_raw) })
-                .for_each(|name| println!("Loaded Vulkan extension [{}]", name.to_str().unwrap()));
+                .map(|property| read_vk_string(&property.extension_name[..]).unwrap())
+                .for_each(|name| println!("Loaded Vulkan extension: {}", name));
+        } else {
+            panic!("Unable to load required platform extensions")
         };
 
+        // Validate validation layers
+        if debug {
+            HelloTriangleApplication::assert_required_layers_available(&entry)
+        };
+
+        let required_validation_layer_raw_names: Vec<CString> = VALIDATION_LAYERS
+            .iter()
+            .map(|layer_name| CString::new(*layer_name).unwrap())
+            .collect();
+        let enabled_layers: Vec<*const i8> = required_validation_layer_raw_names
+            .iter()
+            .map(|layer_name| layer_name.as_ptr())
+            .collect();
+
+        // Create initialisation structs
         let app_name = CString::new(APP_TITLE).unwrap();
         let engine_name = CString::new("Name Pending").unwrap();
         let app_info = ApplicationInfo::builder()
@@ -65,10 +130,23 @@ impl HelloTriangleApplication {
             .api_version(vk::API_VERSION_1_0)
             .build();
 
-        let create_info = InstanceCreateInfo::builder()
-            .application_info(&app_info)
-            .enabled_extension_names(&windows_required_extensions[..]);
+        let create_info = if debug {
+            println!("Creating instance with the following validation layers:");
+            for layer in VALIDATION_LAYERS.iter() {
+                println!("\t{}", layer)
+            }
 
+            InstanceCreateInfo::builder()
+                .application_info(&app_info)
+                .enabled_layer_names(&enabled_layers[..])
+                .enabled_extension_names(&windows_required_extensions[..])
+        } else {
+            InstanceCreateInfo::builder()
+                .application_info(&app_info)
+                .enabled_extension_names(&windows_required_extensions[..])
+        };
+
+        // Create instance
         unsafe {
             entry
                 .create_instance(&create_info, None)
@@ -122,8 +200,10 @@ impl Drop for HelloTriangleApplication {
 }
 
 fn main() {
+    let debug_layers = true;
+
     let event_loop = EventLoop::new();
     let _window = HelloTriangleApplication::init_window(&event_loop);
-    let mut app = HelloTriangleApplication::initialize();
+    let mut app = HelloTriangleApplication::initialize(debug_layers);
     app.run(event_loop);
 }
