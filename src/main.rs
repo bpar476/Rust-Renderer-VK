8df54d65e1dc8b121dbdadbd6e32d937cea41a6d
Ben Partridge <ben.partridge249@gmail.com>
2021-09-19 16:52:58 +1000

    Debut Utils messenger callback
    Sets up the messenger callback for the Debug Utils extensions.
    
    https://www.khronos.org/registry/vulkan/specs/1.1-extensions/html/vkspec.html#VK_EXT_debug_utils
    
    Ash provides a struct `DebugUtils` which loads the debug utils functions
    for us. We define a callback which reads the message severity, message
    type, and message callback and create the message handler object
    associated with our Instance object using the extension's
    `vkCreateDebugUtilsMessengerEXT` function. Additionally we provide the
    same messenger as the `pNext` when creating the instance so it can also
    debug the instance creation and destruction. The messenger is cleaned up
    by the extension as part of the `Drop` implementation of the
    application.
    
    Additionally this commit fixes a bug where `Drop` isn't being called at
    the end of the program. `winit`'s `EventLoop#run` hijacks the main
    thread resulting in resources in the old thread not being dropped. We
    create a placeholder function `draw_frame` which we can call from the
    event loop callback. This ensures it is dropped when the event loop
    exits.
    
    Resources:
    
    - https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Validation_layers
    - https://docs.rs/winit/0.24.0/winit/event_loop/struct.EventLoop.html#method.run
    - https://github.com/unknownue/vulkan-tutorial-rust/blob/master/src/tutorials/02_validation_layers.rs
    - https://github.com/unknownue/vulkan-tutorial-rust/issues/14

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use core::panic;
-use std::ffi::CString;
+use std::ffi::{c_void, CStr, CString};
+use std::ops::{BitAndAssign, Not};
 use std::os::raw::c_char;
 use std::string::FromUtf8Error;
 
@@ -8,12 +9,41 @@ use ash::extensions::khr::{Surface, Win32Surface};
 use ash::vk::{self, ApplicationInfo, InstanceCreateInfo};
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::Window;
 
 const APP_TITLE: &str = "Rust Renderer VK";
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 
 const VALIDATION_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
+
+// Debug utils callback
+unsafe extern "system" fn vulkan_debug_utils_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut c_void,
+) -> vk::Bool32 {
+    let severity = match message_severity {
+        vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "VERBOSE",
+        vk::DebugUtilsMessageSeverityFlagsEXT::INFO => "INFO",
+        vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "WARN",
+        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => "ERROR",
+        _ => "???",
+    };
+    let kind = match message_type {
+        vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "general",
+        vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION => "validation",
+        vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "perf",
+        _ => "???",
+    };
+
+    let message = CStr::from_ptr((*p_callback_data).p_message);
+    eprintln!("[VK DEBUG][{}][{}]: {:?}", severity, kind, message);
+
+    // Return false to indicate that validation should not cause a crash
+    vk::FALSE
+}
 
 fn read_vk_string(chars: &[c_char]) -> Result<String, FromUtf8Error> {
     let terminator = '\0' as u8;
@@ -35,27 +65,111 @@ fn read_vk_string(chars: &[c_char]) -> Result<String, FromUtf8Error> {
 struct HelloTriangleApplication {
     _entry: ash::Entry,
     instance: ash::Instance,
+
+    debug_loader: Option<ash::extensions::ext::DebugUtils>,
+    debug_messenger_ext: Option<vk::DebugUtilsMessengerEXT>,
 }
 
 impl HelloTriangleApplication {
     pub fn initialize(debug: bool) -> Self {
         let entry = unsafe { ash::Entry::new().unwrap() };
         let instance = HelloTriangleApplication::create_instance(&entry, debug);
+
+        let (debug_loader, debug_messenger_ext) = if debug {
+            let (loader, messenger_ext) =
+                HelloTriangleApplication::create_debug_messenger(&entry, &instance);
+            (Some(loader), Some(messenger_ext))
+        } else {
+            (None, None)
+        };
+
         Self {
             _entry: entry,
             instance,
+            debug_loader,
+            debug_messenger_ext,
         }
     }
 
-    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
-        winit::window::WindowBuilder::new()
-            .with_title(APP_TITLE)
-            .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
-            .build(event_loop)
-            .expect("Failed to create window.")
+    fn create_instance(entry: &ash::Entry, debug: bool) -> ash::Instance {
+        let extensions = HelloTriangleApplication::get_extensions(debug);
+
+        // Check available extensions
+        if let Ok(properties) = entry.enumerate_instance_extension_properties() {
+            // TODO check that required extensions are present
+            properties
+                .iter()
+                .map(|property| read_vk_string(&property.extension_name[..]).unwrap())
+                .for_each(|name| println!("Loaded Vulkan extension: {}", name));
+        } else {
+            panic!("Unable to load required platform extensions")
+        };
+
+        // Validate validation layers
+        if debug {
+            HelloTriangleApplication::assert_required_validation_layers_available(&entry)
+        };
+
+        let required_validation_layer_raw_names: Vec<CString> = VALIDATION_LAYERS
+            .iter()
+            .map(|layer_name| CString::new(*layer_name).unwrap())
+            .collect();
+        let enabled_layers: Vec<*const i8> = required_validation_layer_raw_names
+            .iter()
+            .map(|layer_name| layer_name.as_ptr())
+            .collect();
+
+        // Create initialisation structs
+        let app_name = CString::new(APP_TITLE).unwrap();
+        let engine_name = CString::new("Name Pending").unwrap();
+        let app_info = ApplicationInfo::builder()
+            .application_name(&app_name)
+            .application_version(vk::make_api_version(0, 0, 0, 1))
+            .engine_name(&engine_name)
+            .engine_version(vk::make_api_version(0, 0, 0, 1))
+            .api_version(vk::API_VERSION_1_0)
+            .build();
+
+        let mut debug_utils_create_info =
+            HelloTriangleApplication::build_debug_utils_messenger_create_info();
+        let create_info = if debug {
+            println!("Creating instance with the following validation layers:");
+            for layer in VALIDATION_LAYERS.iter() {
+                println!("\t{}", layer)
+            }
+
+            InstanceCreateInfo::builder()
+                .application_info(&app_info)
+                .enabled_layer_names(&enabled_layers[..])
+                .enabled_extension_names(&extensions[..])
+                .push_next(&mut debug_utils_create_info)
+        } else {
+            InstanceCreateInfo::builder()
+                .application_info(&app_info)
+                .enabled_extension_names(&extensions[..])
+        };
+
+        // Create instance
+        unsafe {
+            entry
+                .create_instance(&create_info, None)
+                .expect("Failed to create Vulkan instance")
+        }
     }
 
-    fn assert_required_layers_available(entry: &ash::Entry) {
+    fn get_extensions(debug: bool) -> Vec<*const c_char> {
+        let mut extensions: Vec<*const c_char> = vec![];
+
+        extensions.push(Surface::name().as_ptr());
+        extensions.push(Win32Surface::name().as_ptr());
+
+        if debug {
+            extensions.push(DebugUtils::name().as_ptr());
+        }
+        extensions
+    }
+
+    fn assert_required_validation_layers_available(entry: &ash::Entry) {
         match entry.enumerate_instance_layer_properties() {
             Ok(layers) => {
                 let layer_names: Vec<String> = layers
@@ -86,75 +200,48 @@ impl HelloTriangleApplication {
         }
     }
 
-    fn create_instance(entry: &ash::Entry, debug: bool) -> ash::Instance {
-        // Validate extensions
-        let windows_required_extensions = vec![
-            Surface::name().as_ptr(),
-            Win32Surface::name().as_ptr(),
-            DebugUtils::name().as_ptr(),
-        ];
+    fn build_debug_utils_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
+        let mut severities = vk::DebugUtilsMessageSeverityFlagsEXT::all();
+        severities.bitand_assign(vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE.not());
 
-        // Check available extensions
-        if let Ok(properties) = entry.enumerate_instance_extension_properties() {
-            // TODO check that required extensions are present
-            properties
-                .iter()
-                .map(|property| read_vk_string(&property.extension_name[..]).unwrap())
-                .for_each(|name| println!("Loaded Vulkan extension: {}", name));
-        } else {
-            panic!("Unable to load required platform extensions")
-        };
-
-        // Validate validation layers
-        if debug {
-            HelloTriangleApplication::assert_required_layers_available(&entry)
-        };
-
-        let required_validation_layer_raw_names: Vec<CString> = VALIDATION_LAYERS
-            .iter()
-            .map(|layer_name| CString::new(*layer_name).unwrap())
-            .collect();
-        let enabled_layers: Vec<*const i8> = required_validation_layer_raw_names
-            .iter()
-            .map(|layer_name| layer_name.as_ptr())
-            .collect();
-
-        // Create initialisation structs
-        let app_name = CString::new(APP_TITLE).unwrap();
-        let engine_name = CString::new("Name Pending").unwrap();
-        let app_info = ApplicationInfo::builder()
-            .application_name(&app_name)
-            .application_version(vk::make_api_version(0, 0, 0, 1))
-            .engine_name(&engine_name)
-            .engine_version(vk::make_api_version(0, 0, 0, 1))
-            .api_version(vk::API_VERSION_1_0)
-            .build();
-
-        let create_info = if debug {
-            println!("Creating instance with the following validation layers:");
-            for layer in VALIDATION_LAYERS.iter() {
-                println!("\t{}", layer)
-            }
-
-            InstanceCreateInfo::builder()
-                .application_info(&app_info)
-                .enabled_layer_names(&enabled_layers[..])
-                .enabled_extension_names(&windows_required_extensions[..])
-        } else {
-            InstanceCreateInfo::builder()
-                .application_info(&app_info)
-                .enabled_extension_names(&windows_required_extensions[..])
-        };
-
-        // Create instance
-        unsafe {
-            entry
-                .create_instance(&create_info, None)
-                .expect("Failed to create Vulkan instance")
-        }
+        vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(severities)
+            .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
+            .pfn_user_callback(Some(vulkan_debug_utils_callback))
+            .build()
     }
 
-    fn main_loop(&mut self, event_loop: EventLoop<()>) {
+    fn create_debug_messenger(
+        entry: &ash::Entry,
+        instance: &ash::Instance,
+    ) -> (ash::extensions::ext::DebugUtils, vk::DebugUtilsMessengerEXT) {
+        let create_info = HelloTriangleApplication::build_debug_utils_messenger_create_info();
+        // This DebugUtils struct loads the extension function for us since debug utils are not a part of the standard
+        // they are not loaded when creating the Entry
+        let debug_utils_loader = ash::extensions::ext::DebugUtils::new(entry, instance);
+
+        let messenger = unsafe {
+            debug_utils_loader
+                .create_debug_utils_messenger(&create_info, None)
+                .expect("Debug Utils Callback")
+        };
+
+        (debug_utils_loader, messenger)
+    }
+
+    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
+        winit::window::WindowBuilder::new()
+            .with_title(APP_TITLE)
+            .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
+            .build(event_loop)
+            .expect("Failed to create window.")
+    }
+
+    fn draw_frame(&mut self) {
+        // Drawing will be here
+    }
+
+    fn main_loop(mut self, event_loop: EventLoop<()>, window: Window) {
         event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Poll;
 
@@ -173,6 +260,8 @@ impl HelloTriangleApplication {
                     // You only need to call this if you've determined that you need to redraw, in
                     // applications which do not always need to. Applications that redraw continuously
                     // can just render here instead.
+
+                    window.request_redraw()
                 }
                 Event::RedrawRequested(_) => {
                     // Redraw the application.
@@ -180,20 +269,30 @@ impl HelloTriangleApplication {
                     // It's preferable for applications that do not render continuously to render in
                     // this event rather than in MainEventsCleared, since rendering in here allows
                     // the program to gracefully handle redraws requested by the OS.
+
+                    // NOTE: This function does nothing, however if we don't reference `self` in this loop,
+                    // Drop will never be called for our application.
+                    self.draw_frame();
                 }
                 _ => (),
             }
         });
     }
 
-    fn run(&mut self, event_loop: EventLoop<()>) {
-        self.main_loop(event_loop);
+    fn run(self, event_loop: EventLoop<()>, window: Window) {
+        self.main_loop(event_loop, window);
     }
 }
 
 impl Drop for HelloTriangleApplication {
     fn drop(&mut self) {
         unsafe {
+            if let (Some(loader), Some(messenger)) =
+                // FIXME: Not quite sure why this needs to be a ref
+                (self.debug_loader.as_ref(), self.debug_messenger_ext)
+            {
+                loader.destroy_debug_utils_messenger(messenger, None)
+            }
             self.instance.destroy_instance(None);
         }
     }
@@ -203,7 +302,7 @@ fn main() {
     let debug_layers = true;
 
     let event_loop = EventLoop::new();
-    let _window = HelloTriangleApplication::init_window(&event_loop);
-    let mut app = HelloTriangleApplication::initialize(debug_layers);
-    app.run(event_loop);
+    let window = HelloTriangleApplication::init_window(&event_loop);
+    let app = HelloTriangleApplication::initialize(debug_layers);
+    app.run(event_loop, window);
 }
